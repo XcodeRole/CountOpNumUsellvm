677e8dae1b657ba825a4ab8de5fc6bdb37873356
XcodeRole <XcodeRole@qq.com>
2022-04-01 17:35:21 +0800

    init

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-make clean && make && opt -load ./CountOpCode.so -CountOpCode -enable-new-pm=0 main.bc
+make clean && make && opt -load ./CountOpCode.so -CountOpCode -enable-new-pm=0 test.bc
